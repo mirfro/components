301cdc0b1279d90f61932e4371d3e04148df021f
Rubén Maguregui <ruben.maguregui@gmail.com>
2025-01-08 20:28:29 -0600

    feat: naively implement mir-masked-input

--- packages/web-components/dist/src/inputs/mir-masked-input.d.ts
+++ packages/web-components/dist/src/inputs/mir-masked-input.d.ts
@@ -1,2 +1,7 @@
 export declare class MirMaskedInput extends HTMLInputElement {
+    #private;
+    connectedCallback(): void;
+    disconnectedCallback(): void;
+    attributeChangedCallback(name: string, prev: unknown, value: string): void;
+    patternChanged(prev: unknown, value: string): void;
 }
--- packages/web-components/src/inputs/mir-masked-input.ts
+++ packages/web-components/src/inputs/mir-masked-input.ts
@@ -1,4 +1,27 @@
-export class MirMaskedInput extends HTMLInputElement {
+import IMask, { InputMask } from 'imask';
 
+export class MirMaskedInput extends HTMLInputElement {
+    #masker: InputMask;
+
+    connectedCallback() {
+        this.#masker = IMask(this, {
+            mask: this.getAttribute('pattern'),
+        });
+    }
+    disconnectedCallback() {
+        this.#masker.destroy();
+    }
+
+    attributeChangedCallback(name: string, prev: unknown, value: string) {
+        if (this[`${name}Changed`]) {
+            this[`${name}Changed`](prev, value);
+        }
+    }
+
+    patternChanged(prev: unknown, value: string) {
+        if (this.#masker) {
+            this.#masker.mask = value;
+        }
+    }
 }
 customElements.define('mir-masked-input', MirMaskedInput);
